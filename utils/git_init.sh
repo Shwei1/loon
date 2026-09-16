b38ef34bd57549ba99f36ab5da94d1a158f69126
Shwei <portal.gamy@gmail.com>
2026-09-16 21:38:46 +0300

    Add take_input function

--- utils/git_init.sh
+++ utils/git_init.sh
@@ -1,18 +1,49 @@
 #!/usr/bin/env bash
 
-if [ ! -d ".git" ]; then
+take_input() {
+    read -pr "username: " USER_NAME
+    read -pr "email: " USER_EMAIL
+    read -pr "default branch name: " USER_BRANCH
+
+    export USER_NAME USER_EMAIL USER_BRANCH
+}
+
+
+if [ -d ".git" ]; then
     echo "Current directory already is a git repository. Aborting." >&2
     exit 1
 fi
 
+
+if [ ! -f ".git_myconfig" ]; then
+    while true; do
+        read -pr -n 1 "Current directory does not have a configuration file. Create one? [y/n] " ans
+        case "$ans" in
+            y|Y)
+                take_input 
+                break
+                ;;
+            n|N)
+                echo "No idea what to do."
+                exit 1
+                ;;
+            *)
+                echo "Enter y or n."
+                
+        esac
+    done
+fi
+
+
+
 case "$#" in
-    1)
+    0)
         echo "Called with no args"
         ;;
-    2)
+    1)
         echo "Called with 1 arg"
         ;;
-    3)
+    2)
         echo "Called with 2 args"
         ;;
     *)
