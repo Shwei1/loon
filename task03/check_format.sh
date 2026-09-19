f9fb207309fe1d7d9e06ebf6f077449231ca2ebd
shwi <shwe>
2026-09-20 02:00:47 +0300

    TASK 03: Add format checking script, tested

---
+++ task03/check_format.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+print_help() {
+    echo "Output clang formatter result"
+    echo "Usage: ./check_format.sh [ROOT-DIR]"
+}
+
+print_format() {
+    echo "==== CLANG-FORMAT VERSION 11 ===="
+    if clang-format-11 --dry-run -Werror -style=file "$1"/src/*.c "$1"/include/*.h > /dev/null 2>&1; then
+        echo "All filles are formatted well"
+    else
+        echo "Some files are formatted incorrectly"
+    fi
+   
+    echo "================================="
+
+    echo
+
+    # Остання версія на моїй системі
+    local clang_format_version
+    clang_format_version="$(clang-format --version | cut -d ' ' -f 3)"
+
+    echo "==== CLANG-FORMAT VERSION $clang_format_version ===="
+    if clang-format --dry-run -Werror -style=file "$1"/src/*.c "$1"/include/*.h  > /dev/null 2>&1; then
+        echo "All filles are formatted well"
+    else
+        echo "Some files are formatted incorrectly"
+    fi
+    echo "================================="
+}
+
+
+case "$#" in
+    0)
+        print_help
+        exit 0
+        ;;
+    1)
+        print_format "$1"
+        ;;
+    *)
+        echo "Too many arguments"
+        print_help
+        exit 1
+esac
+
+
